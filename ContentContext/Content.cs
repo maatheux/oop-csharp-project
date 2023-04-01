4ce473ac527e00608060b57419be2961dd6e3656
Matheus Lima <matheusmarinho.lima@hotmail.com>
2023-03-31 21:14:24 -0300

    Heranca e classe abstrata

--- ContentContext/Content.cs
+++ ContentContext/Content.cs
@@ -1,8 +1,13 @@
 namespace Balta.ContentContext;
 
-public class Content
+public abstract class Content
 {
-  public int Id { get; set; }
-  public string Title { get; set; }
-  public string Url { get; set; }
+  public Content()
+  {
+    Id = Guid.NewGuid(); // SPOF -> Single Point of Fail
+  }
+
+  public Guid Id { get; set; }
+  public string? Title { get; set; }
+  public string? Url { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Balta;
+﻿using Balta.ContentContext;
+
+namespace Balta;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var course = new Course();
     }
 }
